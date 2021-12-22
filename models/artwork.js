ff5598bfec92480d58b43a6d8178863146200f58
Midori Kocak <mtkocak@gmail.com>
2021-12-22 21:41:48 +0100

    things TODO:

--- models/artwork.js
+++ models/artwork.js
@@ -1,40 +1,5 @@
-let data = [{
-    id: 1,
-    name: "Starry Night",
-    description: "The Starry Night is an oil-on-canvas painting by the Dutch Post-Impressionist painter Vincent van Gogh. Painted in June 1889, it depicts the view from the east-facing window of his asylum room at Saint-Rémy-de-Provence, just before sunrise, with the addition of an imaginary village.",
-    format: "Oil on Canvas",
-    artist: "Vincent Van Gogh",
-    year: "1899",
-    width: "73",
-    length: "32",
-    created: "2021-12-18 12:14",
-    updated: "2021-12-18 12:14",
-  },
-  {
-    id: 2,
-    name: "Sunflowers",
-    description: "Yellow Sunflowers",
-    format: "Oil on Canvas",
-    artist: "Vincent Van Gogh",
-    year: "1899",
-    width: "73",
-    length: "60",
-    created: "2021-12-18 12:14",
-    updated: "2021-12-18 12:14",
-  },
-  {
-    id: 3,
-    name: "Self Portrait",
-    description: "Ear Cut",
-    format: "Oil on Canvas",
-    artist: "Vincent Van Gogh",
-    year: "1899",
-    width: "73",
-    length: "40",
-    created: "2021-12-18 12:14",
-    updated: "2021-12-18 12:14",
-  },
-];
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database('./data/data.db');
 
 class Artwork {
   constructor(id, name, format, artist, year, width, length) {
@@ -47,45 +12,33 @@ class Artwork {
     this.length = length;
   }
 
-  static one(id) {
-    return data.find((item) => item.id == id);
+  // Completed
+  static one(id, callback) {
+    const sql = 'SELECT *  FROM artworks WHERE id = ?';
+    db.get(sql, id, callback);
   }
 
-  static all() {
-    return data;
+  // Completed
+  static all(callback) {
+    db.all('SELECT * FROM artworks', callback);
   }
 
-  static add(dataObject) {
-    let idArray = data.map((item) => item.id);
-    let maxID = Math.max(...idArray);
-    let newID = maxID + 1;
-    dataObject.id = newID;
-    data.push(dataObject);
+  // TODO: Change fields 
+  static add(artwork) {
+    const sql = 'INSERT INTO artworks(title, status) VALUES(?, ?)';
+    db.run(sql, [todo.title, todo.status]);
   }
 
-  static update(id, dataObject) {
-
-    for (let i = 0; i < data.length; i++) {
-      let currentItem = data[i];
-      if (currentItem.id == id) {
-        currentItem = dataObject;
-        break;
-      }
-    }
+  // TODO: Change fields 
+  static update(artwork, callback) {
+    const sql = 'UPDATE artworks SET title = ?, status = ? WHERE id = ?';
+    db.run(sql, [artwork.title, artwork.status, artwork.id], callback);
   }
 
-  static delete(id) {
-    let arrayIndexToDelete;
-    for (let i = 0; i < data.length; i++) {
-      let currentItem = data[i];
-      if (currentItem.id == id) {
-        arrayIndexToDelete = i;
-        break;
-      }
-    }
-    if (typeof arrayIndexToDelete !== 'undefined') {
-      data.splice(arrayIndexToDelete, 1);
-    }
+  // Complete
+  static delete(id, callback) {
+    const sql = 'DELETE FROM artworks where id = ?';
+    db.run(sql, id, callback);
   }
 }
 
--- routes/artworks.js
+++ routes/artworks.js
@@ -2,27 +2,76 @@ var express = require("express");
 var router = express.Router();
 var Artwork = require("../models/artwork")
 
+// READ
 /* GET artworks listing. */
+// TODO: We should update correct error code
 router.get("/:id", function (req, res, next) {
   const id = req.params.id
-  let data = Artwork.one(id);
-
-  res.format({
-    html: function () {
-      res.render("artwork", data);
-    },
-    json: function () {
-      res.status(200).json(data);
-    },
+  Artwork.one(id, function(err, artwork){
+    if (err) {
+      response.status(404, "The artwork is not found").send();
+    } else {
+      res.format({
+        html: function () {
+          console.log(artwork)
+          res.render("artwork", artwork);
+        },
+        json: function () {
+          res.status(200).json(artwork);
+        },
+      });
+    }
   });
 });
 
+// TODO: Update the router, if there is an error, send the correct status
 router.get("/", function (req, res, next) {
-  let allArtworks = Artwork.all();
-  res.format({
-    json: function () {
-      res.status(200).json(allArtworks);
-    },
+  Artwork.all(function(err, artworks){
+    res.format({
+      json: function () {
+        res.status(200).json(artworks);
+      },
+    });
+  })
+});
+
+// CREATE
+// I think we need the callback here
+// what if we had some error in the model side
+// TODO: Update the router, if there is an error, send the correct status
+router.post("/", function (req, res, next) {
+  const newArtwork = req.body;
+  Artwork.add(newArtwork);
+  response.status(201).json();
+});
+
+// UPDATE
+// TODO: We should update correct error code
+// TODO: Request should be req to be compatible with other functions
+router.put("/:id", (request, response) => {
+  const id = request.params.id;
+  const updatedArtwork = request.body;
+  updatedArtwork.id = parseInt(id);
+  Artwork.update(updatedArtwork, (err, data) => {
+    if (err) {
+      response.status(404, "The artwork is not found").send();
+    } else {
+      response.status(204).send(data);
+    }
+  });
+});
+
+// DELETE
+// TODO: We should update correct error code
+router.delete("/:id", (request, response) => {
+  const id = parseInt(request.params.id);
+  Artwork.delete(id, (err) => {
+    console.log(err)
+    if (err) {
+      response.status(404).send();
+    } else {
+      response.status(200).send();
+    }
   });
 });
 
